2e1bbb828644c0b352107ded1261a6c83832e300
YouOne22 <142289104+YouOne22@users.noreply.github.com>
2026-07-21 17:26:48 +0700

    Update main.py

--- main.py
+++ main.py
@@ -48,7 +48,7 @@ def generate_prompt(req: PromptRequest):
 
     try:
         response = client.models.generate_content(
-            model="gemini-1.5-flash",
+            model="gemini-2.0-flash",
             contents=user_content,
             config={"system_instruction": system_instruction}
         )
