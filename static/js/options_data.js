4fe250e477ddfbf6254842263800617e1494c77a
YouOne22 <142289104+YouOne22@users.noreply.github.com>
2026-07-21 21:36:12 +0700

    Create options_data.js

---
+++ static/js/options_data.js
@@ -0,0 +1,36 @@
+const subStyleOptions = {
+    "Spanduk": [
+        "Pengajian / Keagamaan",
+        "Formal (Rapat, Resmi, dan Lain-lain)",
+        "Pengumuman / Informasi Publik",
+        "Promosi Produk / Iklan Bisnis",
+        "Lainnya"
+    ],
+    "Poster": [
+        "Formal (Rapat, Resmi, dan Lain-lain)",
+        "Pengajian / Keagamaan",
+        "Promosi Produk / Iklan Bisnis",
+        "Lainnya"
+    ],
+    "Stiker": [
+        "Formal / Pelabelan Produk",
+        "Pengajian / Komunitas",
+        "Promosi / Merchandise Bisnis",
+        "Lainnya"
+    ],
+    "Kartu Nama": [
+        "Pengacara / Konsultan Hukum",
+        "Bengkel / Otomotif & Modifikasi",
+        "Perusahaan / Korporat Profesional",
+        "Lainnya"
+    ],
+    "Lainnya": ["Umum / Kustom Bebas"]
+};
+
+const sizePresetOptions = {
+    "Spanduk": ["3 x 1 meter", "2 x 1 meter", "4 x 1 meter", "Custom..."],
+    "Poster": ["A4 (21 x 29.7 cm)", "A3 (29.7 x 42 cm)", "Custom..."],
+    "Stiker": ["5 x 5 cm", "7 x 7 cm", "10 x 10 cm", "Custom..."],
+    "Kartu Nama": ["9 x 5.5 cm (Standar Indonesia)", "9 x 5 cm (Slim)", "Custom..."],
+    "Lainnya": ["1080 x 1080 px", "1920 x 1080 px", "Custom..."]
+};
