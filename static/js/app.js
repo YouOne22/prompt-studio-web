b6879c1f5dff24af71d42212e63d8e9e2424313b
YouOne22 <142289104+YouOne22@users.noreply.github.com>
2026-07-22 17:18:25 +0700

    Update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -189,8 +189,7 @@ INSTRUKSI KHUSUS OPTIMASI PROMPT GAMBAR:
                         content: metaPromptText
                     }
                 ],
-                temperature: 0.7,
-                max_tokens: 1500
+                temperature: 0.7
             })
         });
 
